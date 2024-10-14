5dbd9742788a23333f1f340545b84f504dcc7db3
root <root@DESKTOP-CKG6LAF.localdomain>
2024-10-14 20:59:09 +0500

    Добавленно именование внутренних процессов

--- programms/yandex-music-Non-official.sh
+++ programms/yandex-music-Non-official.sh
@@ -3,18 +3,25 @@
 # Создание папки для скачивания билда
 if [ ! -d ~/programms-deb ]; then
     mkdir ~/programms-deb
-    echo "Папка programms-deb создана"
+    echo "[Install Programms] Папка programms-deb создана | The programms-deb folder has been created"
 else
-    echo "Папка programms-deb уже создана"
+    echo "[Install Programms] Папка programms-deb уже была создана | The programms-deb folder has already been created"
 fi
 
+# Удаление старого файла если есть
 if [ -e ~/programms-deb/yandex-music-Non-official.deb ]; then
     rm ~/programms-deb/yandex-music-Non-official.deb
-    echo "Старый файл удалён, идёт скачивание нового файла"
+    echo "[Install Programms] Старый файл удалён, идёт скачивание нового файла | The old file has been deleted, and a new one is being downloaded"
 fi
 
 # Получение билда из git
 wget -O ~/programms-deb/yandex-music-Non-official.deb https://github.com/cucumber-sp/yandex-music-linux/releases/download/v5.22.1/yandex-music_5.22.1_amd64.deb
 
+# Очистка консоли
+clear
+
 # Установка программы
 sudo apt install ~/programms-deb/yandex-music-Non-official.deb
+
+# Очистка консоли
+clear
